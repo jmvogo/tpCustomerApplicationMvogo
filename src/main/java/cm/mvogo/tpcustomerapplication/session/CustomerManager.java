9d33580df0c619f648c1dbdbab8b1170072daf38
Mvogo Ngono Joseph <joseph.mvogo@gmail.com>
2022-04-13 07:54:47 +0200

    Ajout de l'EJB customerManager

---
+++ src/main/java/cm/mvogo/tpcustomerapplication/session/CustomerManager.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package cm.mvogo.tpcustomerapplication.session;
+
+import cm.mvogo.tpcustomerapplication.entities.Customer;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ * Gère la persistance des Customers.
+ */
+
+@Stateless
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {
+       Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+
+    public void persist(Customer customer) {
+       em.persist(customer);
+    }
+}
